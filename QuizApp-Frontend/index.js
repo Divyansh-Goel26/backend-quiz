26b8f3a9724525f7232335731d2782cb52bc3e64
Divyansh-Goel26 <110087069+Divyansh-Goel26@users.noreply.github.com>
2025-03-06 13:02:10 +0530

    Delete QuizApp-Frontend directory

--- QuizApp-Frontend/index.js
+++
@@ -1,9 +0,0 @@
-function startQuiz(quizType){
-    // alert(`Starting ${quizType.charAt(0).toUpperCase() + quizType.slice(1)} Quiz!`);
-    
-    localStorage.setItem("quizType", quizType);
-    alert(`Starting ${quizType.toUpperCase()} Quiz! `)
-    
-    window.location.href = `quizPage.html`;
-    // window.location.href = quizPage.html;
-}
